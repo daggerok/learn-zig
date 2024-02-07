7cc837045dee4b6cd10b7b38d7eb48ed7699ccb5
Maksim Kostromin <daggerok@gmail.com>
2024-02-06 23:38:05 -0500

    Update build.zig

--- zig-01-hello/build.zig
+++ zig-01-hello/build.zig
@@ -1,7 +1,7 @@
 // https://ziglang.org/learn/build-system/
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_source_file = .{ .path = "hello.zig" },
